4c9052ae39f2c304253be330977072d5e68f3da2
CCYQ <ccyq1994@163.com>
2024-06-05 16:38:48 +0800

    add some ton function

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './manifest.json'
+import { TonConnectButton, useTonWallet, useTonAddress } from '@tonconnect/ui-react'
 import WebApp from '@twa-dev/sdk'
-import { TonConnectButton, TonConnectUIProvider, useTonWallet } from '@tonconnect/ui-react'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+export const Address = () => {
+  const userAddress = useTonAddress();
+  const rawAddress = useTonAddress(false);
+  return (
+    userAddress && (
+      <div>
+        <span>User-Friendly address: {userAddress}</span>
+        <span>Raw address: {rawAddress}</span>
+      </div>
+    )
+  );
+}
+
+export const Wallet = () => {
   const wallet = useTonWallet();
   return (
+    wallet && (
+      <div>
+        <span>Connected wallet: {wallet?.account.address}</span>
+        <span>Device: {wallet?.device.appName}</span>
+      </div>
+    )
+  );
+}
+
+function App() {
+
+
+
+  return (
     <>
-      <TonConnectUIProvider 
-      manifestUrl='https://ccyq1995.github.io/TeMiniApp_Web_App/manifest.json' 
-      actionsConfiguration={{twaReturnUrl: 'https://t.me/Xsy89757Bot'}}>
+      <TonConnectButton className='ton-connect-class' />
+
+      <Address />
+
+      <Wallet></Wallet>
+
+      <button onClick={() => WebApp.showAlert(`Hello World!}`)} >
+        show WebApp Alert
+      </button>
+
+      <button onClick={() => { }}>
+        Connected Wallet
+      </button>
 
 
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        wallet && (
-          <div>
-            <span>Connected wallet: {wallet?.account.address}</span>
-            <span>Device: {wallet?.device.appName}</span>
-          </div>
-        )
-
-
-        <div className='card'>
-          <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)} >
-            show WebApp Alert
-          </button>
-
-          <TonConnectButton className='wallet-button'/>
-
-
-        </div>
-
-
-
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </TonConnectUIProvider>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import WebApp from '@twa-dev/sdk'
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TonConnectUIProvider
+      manifestUrl="https://ccyq1995.github.io/TeMiniApp_Web_App/manifest.json"
+      actionsConfiguration={{ twaReturnUrl: 'https://t.me/Xsy89757Bot' }}
+    >
+      <App />
+    </TonConnectUIProvider>
   </React.StrictMode>,
 )
