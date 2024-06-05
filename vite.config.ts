3acec65654df6e741811a8105211a890d93a8a24
CCYQ <ccyq1994@163.com>
2024-06-05 11:58:27 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  build: {
+    outDir: './dist'
+  },
   base: './'
 })
