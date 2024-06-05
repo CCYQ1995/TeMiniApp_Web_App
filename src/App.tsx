61d9e3e057386c4096f1922dd3be22de5480b385
CCYQ <ccyq1994@163.com>
2024-06-05 18:04:47 +0800

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useCallback } from 'react';
+import { TonConnectButton } from '@tonconnect/ui-react';
 // import { useTonWallet, useTonAddress } from '@tonconnect/ui-react'
 // import WebApp from '@twa-dev/sdk'
 
 import './App.css'
 import { Connector } from './Connector';
+
 // import { isDesktop, isMobile, openLink } from './Utils';
 
 
@@ -13,13 +15,16 @@ function App() {
 
   const connectDidClick = useCallback(() => {
     console.log(walletsList);
+    Connector.connect
+
   }, [walletsList]);
 
   return (
     <div className='app'>
-      <header>
+      <header className='app-header'>
         <span className='app-title'>My App</span>
-        <button onClick={connectDidClick}>Connect Wallet</button>
+        {/* <button onClick={connectDidClick}>Connect Wallet</button> */}
+        <TonConnectButton></TonConnectButton>
       </header>
 
       <main>
