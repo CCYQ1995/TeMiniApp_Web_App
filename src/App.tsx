9ac329a09b6e40ebcd7a35e1343f842fa5434a47
CCYQ <ccyq1994@163.com>
2024-06-05 15:46:33 +0800

    add some ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,58 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
+import { TonConnectButton, TonConnectUIProvider, useTonWallet } from '@tonconnect/ui-react'
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const wallet = useTonWallet();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <TonConnectUIProvider actionsConfiguration={{twaReturnUrl: 'https://t.me/Xsy89757Bot'}}>
+
+
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        wallet && (
+          <div>
+            <span>Connected wallet: {wallet?.account.address}</span>
+            <span>Device: {wallet?.device.appName}</span>
+          </div>
+        )
+
+
+        <div className='card'>
+          <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)} >
+            show WebApp Alert
+          </button>
+
+          <TonConnectButton className='wallet-button'/>
+
+
+        </div>
+
+
+
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <div className='card'>
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)} />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </TonConnectUIProvider>
     </>
   )
 }
